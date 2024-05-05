4e099e091574741ee8411143225591c408212c69
IamShaDoW666 <www.gaming2018@gmail.com>
2024-05-05 21:34:29 +0530

    new buid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://dubai-hikvision.com',
+  integrations: [tailwind(), react(), sitemap()]
 });
