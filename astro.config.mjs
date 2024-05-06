343e2867fd2176d532b95c75f1c993f0ca6bfb1a
IamShaDoW666 <www.gaming2018@gmail.com>
2024-05-06 08:16:44 +0530

    new build google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://dubai-hikvision.com',
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ]
 });
