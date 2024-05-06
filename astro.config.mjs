1a45a4d1347584331d47c2ea1c67cbb62b7823b5
IamShaDoW666 <www.gaming2018@gmail.com>
2024-05-06 22:25:46 +0530

    new build compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
+import playformCompress from "@playform/compress";
 
 export default defineConfig({
   site: 'https://dubai-hikvision.com',
-  integrations: [tailwind(), react(), sitemap(),
-    partytown({
-			config: {
-        textCompression: 'gzip',
-			  forward: ["dataLayer.push"],
-       
-			},
-		}),
-  ]
+  integrations: [tailwind(), react(), sitemap(), partytown({
+    config: {
+      
+      forward: ["dataLayer.push"]
+    }
+  }), playformCompress()]
 });
