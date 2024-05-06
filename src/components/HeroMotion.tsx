96f96f530476f4ba7b9b48b3e61373a316932f67
IamShaDoW666 <www.gaming2018@gmail.com>
2024-05-06 18:09:58 +0530

    animation change

---
+++ src/components/HeroMotion.tsx
@@ -0,0 +1,20 @@
+import { motion, spring } from "framer-motion";
+
+const Hero = () => {
+  return (<>
+  <div className="lg:ms-8">
+    <div className="atropos relative">
+      <div className="atropos-scale">
+        <motion.div
+          className="container"
+          initial={{  scale: 0.9 }}
+          animate={{  scale: 1 }}
+          transition={{repeat: Infinity, repeatType: "mirror", duration: 2}}
+        >
+          <img src="assets/images/HIK.svg" />
+        </motion.div>
+      </div>
+    </div>
+  </div></>)
+};
+export default Hero;
