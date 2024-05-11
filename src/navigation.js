64a5f9da2931bbb534640632edba4a07f8151e27
nibhapr <nibhapr@gmail.com>
2024-05-11 14:30:43 +0530

    new build

--- src/navigation.js
+++ src/navigation.js
@@ -6,7 +6,7 @@ export const socialLinks = {
   x: "https://twitter.com",
   linkedin: "https://www.linkedin.com/company/digital-link-technology-uae/",
   google: "https://g.co/kgs/yq4CrN8",
-  instagram: "https://www.instagram.com/digitallinktechnologyllc/",
+  instagram: "https://www.instagram.com/reel/C6qFHCHvfTu/?utm_source=ig_web_copy_link",
 };
 export const navBarLinks = [
   { name: "Home", url: "/" },
