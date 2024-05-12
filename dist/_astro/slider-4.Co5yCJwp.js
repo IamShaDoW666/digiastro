dc2293680c9eea6373fccf5356e0695fd183df08
IamShaDoW666 <www.gaming2018@gmail.com>
2024-05-12 18:06:43 +0530

    new build

---
+++ dist/_astro/slider-4.Co5yCJwp.js
@@ -0,0 +1,1 @@
+const e="slider-4.md",s="slider",t="slider-4",i="",n={id:4,slider:"../slider/hik2.webp",sliderAlt:"seamless Integration:"},d={type:"content",filePath:"C:/Users/Milan/Desktop/Work/digiastro/src/content/slider/slider-4.md",rawData:void 0};export{d as _internal,i as body,s as collection,n as data,e as id,t as slug};
--- dist/_astro/slider-5.DFLhj7Y4.js
+++
@@ -1,1 +0,0 @@
-const t="slider-5.md",e="slider",s="slider-5",d="",i={id:5,slider:"../slider/hik1.webp",sliderAlt:"seamless Integration:"},n={type:"content",filePath:"C:/xampp/htdocs/digiastro/src/content/slider/slider-5.md",rawData:void 0};export{n as _internal,d as body,e as collection,i as data,t as id,s as slug};
