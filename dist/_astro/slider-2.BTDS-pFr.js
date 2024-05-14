340760056457b04f42e6d7ab6e4a2b9d1205c4d8
unknown <nibhapr@gmail.com>
2024-05-14 17:46:37 +0530

    new build

--- dist/_astro/slider-2.BTDS-pFr.js
+++
@@ -1,1 +0,0 @@
-const e="slider-2.md",s="slider",t="slider-2",i="",n={id:2,slider:"../slider/hik8.webp",sliderAlt:"seamless Integration:"},d={type:"content",filePath:"C:/Users/Milan/Desktop/Work/digiastro/src/content/slider/slider-2.md",rawData:void 0};export{d as _internal,i as body,s as collection,n as data,e as id,t as slug};
